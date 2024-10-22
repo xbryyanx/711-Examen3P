e1235c9c573352912a931946224aa2d82f5f277a
xbryyanx <bryanmontoya838@gmail.com>
2024-10-21 18:22:34 -0600

    variable fernanda

--- modules/vm/main.tf
+++ modules/vm/main.tf
@@ -60,6 +60,19 @@ resource "azurerm_network_security_group" "IN_SG" {
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
+
+    security_rule {
+    name                       = "SQlServer-allow"
+    priority                   = 104
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "1433"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+  
   
 }
 
